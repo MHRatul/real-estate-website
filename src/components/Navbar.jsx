b20ce4e084dc9eaf9a159a68828a82f9b4b1aa35
Mahmudul-Hassan-Ratul <mahmudulhassanratul.dev@gmail.com>
2024-12-31 01:22:49 +0600

    Navbar implement & added sign up button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import {assets} from '../assets/assets'
 const Navbar = () => {
   return (
-    <div></div>
+    <div className='absolute top-0 left-0 w-full z-10'>
+        <div className='container mx-auto flex justify-between items-center py-4 px-6 md:px-20 lg:px-32 bg-transparent'>
+            <img src={assets.logo} alt='logo' />
+            <ul className='hidden md:flex bg-white space-x-4'>
+                <a href='#Header' className='cursor-pointer hover:text-gray-400'>Home</a>
+                <a href='#Header' className='cursor-pointer hover:text-gray-400'>About</a>
+                <a href='#Header' className='cursor-pointer hover:text-gray-400'>Projects</a>
+                <a href='#Header' className='cursor-pointer hover:text-gray-400'>Testimonials</a>
+            </ul>
+            <button className='hidden md:block text-white bg-gradient-to-r from-cyan-500 to-blue-500 px-4 py-2 rounded-full'>Sign Up</button>
+        </div>
+    </div>
   )
 }
 
