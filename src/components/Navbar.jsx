364ee763ed0b06972188a72e198bf69b6d4f9a45
Mahmudul-Hassan-Ratul <mahmudulhassanratul.dev@gmail.com>
2024-12-31 10:34:03 +0600

    Header implement & added sign up buttn

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <div className='absolute top-0 left-0 w-full z-10'>
         <div className='container mx-auto flex justify-between items-center py-4 px-6 md:px-20 lg:px-32 bg-transparent'>
             <img src={assets.logo} alt='logo' />
-            <ul className='hidden md:flex bg-white space-x-4'>
+            <ul className='hidden md:flex text-white gap-7'>
                 <a href='#Header' className='cursor-pointer hover:text-gray-400'>Home</a>
                 <a href='#Header' className='cursor-pointer hover:text-gray-400'>About</a>
                 <a href='#Header' className='cursor-pointer hover:text-gray-400'>Projects</a>
