d68d4258709a026206a50c5ce918f7f9999d2e92
Mahmudul-Hassan-Ratul <mahmudulhassanratul.dev@gmail.com>
2024-12-31 20:15:18 +0600

    Project section button addded

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Projects = () => {
   return (
@@ -6,6 +7,17 @@ const Projects = () => {
         <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>Projects <span
          className='underline underline-offset-4 decoration-1 under font-light'>Completed</span></h1>
         <p className=' text-center text-gray-500 max-w-80  mb-8 mx-auto'>Crafting Spaces, Building Legacies-Epic Real Estate Project Portfolio</p>
+
+        {/* Slider buttons */}
+
+        <div className='flex justify-end items-center mb-8'>
+          <button className='p-3 bg-gray-200 rounded mr-2 aria-label="Previous Project'>
+            <img src={assets.left_arrow} alt='Previous' />
+          </button>
+          <button className='p-3 bg-gray-200 rounded mr-2 aria-label="Next Project'>
+            <img src={assets.right_arrow} alt='Next' />
+          </button>
+        </div>
     </div>
   )
 }
