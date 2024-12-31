0c16279386b6cfcf92a2e52653a554cfe68a4930
Mahmudul-Hassan-Ratul <mahmudulhassanratul.dev@gmail.com>
2024-12-31 21:12:36 +0600

    useSatate hook use for card ,slider

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,6 +2,17 @@ import React from 'react'
 import { assets, projectsData } from '../assets/assets'
 
 const Projects = () => {
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [cardsToShow, setCardToShow] = useState(1);
+
+  const nextProject = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % projectsData.length);
+  };
+  const prevProject = () => {
+    setCurrentIndex((prevIndex) => prevIndex === 0 ? projectsData.length - 1 : prevIndex - 1 );
+  };
+
   return (
     <div className='container mx-auto py-4 pt-20 px-6 md:px-20 lg:px-32 my-20 w-full overflow-hidden' id='Projects'>
         <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>Projects <span
@@ -11,10 +22,12 @@ const Projects = () => {
         {/* Slider buttons */}
 
         <div className='flex justify-end items-center mb-8'>
-          <button className='p-3 bg-gray-200 rounded mr-2 aria-label="Previous Project'>
+          <button onClick={prevProject}
+          className='p-3 bg-gray-200 rounded mr-2 aria-label="Previous Project'>
             <img src={assets.left_arrow} alt='Previous' />
           </button>
-          <button className='p-3 bg-gray-200 rounded mr-2 aria-label="Next Project'>
+          <button onClick={nextProject}
+          className='p-3 bg-gray-200 rounded mr-2 aria-label="Next Project'>
             <img src={assets.right_arrow} alt='Next' />
           </button>
         </div>
